57c85cf4e44d3fcff9a7da0d8651a48afa32807a
Felipe <felipedourado920@gmail.com>
2022-06-03 16:38:16 -0300

    finalizado

---
+++ App.js
@@ -0,0 +1,77 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
+import Cabecalho from './src/components/cabecalho';
+import Botao from './src/components/botao';
+import DADOS from './src/dados/dadosJogos';
+import Jogos from './src/components/jogos';
+import Jogos2 from './src/components/jogos';
+import Destaques from './src/components/destaque';
+import NOVOS from './src/dados/novosJogos';
+import Novos from './src/components/novos'
+import Lancamentos from './src/lancamentos';
+
+export default function App() {
+  return (
+    <View>
+      <Cabecalho></Cabecalho>
+      <View style={{marginHorizontal:10},{marginTop:10}}>
+        
+      <Botao
+      Logo= "game-controller"
+      texto="Jogos Gratuitos"
+      cor="black"
+      Logo2= "book"
+      texto2="Livros"
+      Cor2="red"
+      />
+
+    <Botao
+      Logo= "ios-headset-sharp"
+      texto="Músicas"
+      cor="blue"
+      Logo2= "ios-logo-android"
+      texto2="Apps"
+      Cor2="orange"
+      />
+
+    <Botao
+      Logo= "videocam"
+      texto="Filmes"
+      cor="purple"
+      Logo2= "game-controller"
+      texto2="Jogos Pagos"
+      Cor2="gray"
+      />
+</View>
+<Destaques></Destaques>
+<FlatList
+      horizontal = {true}
+      data = {DADOS}
+      keyExtractor = {(item) => item.id}
+      renderItem = { ({item}) => (
+
+        <Jogos
+        titulo={item.nome}
+        imagem={item.imagem}
+        valor={item.valor}
+        />
+      )}
+      />
+    <Lancamentos></Lancamentos>  
+    <FlatList
+      horizontal = {true}
+      data = {NOVOS}
+      keyExtractor = {(item) => item.id}
+      renderItem = { ({item}) => (
+
+        <Novos
+        titulo={item.nome}
+        imagem={item.imagem}
+        valor={item.valor}
+        />
+      )}
+      />
+
+    </View>
+  );
+}
---
+++ src/dados/dadosJogos.js
@@ -0,0 +1,63 @@
+const JOGOS = [
+    {
+        id :"01",
+        nome: "Subway Surfres",
+        valor: "R$13,32",
+        imagem: 'subway.jpg',
+    },
+    {
+        id:"02",
+        nome:"Yugioh Master Duel",
+        valor: "Gratuito",
+        imagem: "yugi.jpg"
+    },
+    {
+        id:"03",
+        nome:"Pokemon Go",
+        valor:"Gratuito",
+        imagem:"pokemon.jpg"
+    },
+    {
+        id:"04",
+        nome:"Hora de aventura",
+        valor:"R$10,00",
+        imagem:"aventura.jpg",
+    },
+    {
+        id:"05",
+        nome:"Dragon Ball Fight",
+        valor:"R$!8,00",
+        imagem:"dbz.jpg",
+    },
+    {
+        id:"06",
+        nome:"FIFA Mobile",
+        valor:"Gratuito",
+        imagem:"fifa.jpg",
+    },
+    {
+        id:"07",
+        nome:"Pou",
+        valor:"Gratuito",
+        imagem:"pou.png",
+    },
+    {
+        id:"08",
+        nome:"Clash Royale",
+        valor:"Gratuito",
+        imagem:"clash.jpg",
+    },
+    {
+        id:"09",
+        nome:"Clash Of Clans",
+        valor:"Gratuito",
+        imagem:"clans.jpg",
+    },
+    {
+        id:"10",
+        nome:"8 Ball Poll",
+        valor:"Gratuito",
+        imagem:"ball.jpg",
+    },
+]
+export default JOGOS;
